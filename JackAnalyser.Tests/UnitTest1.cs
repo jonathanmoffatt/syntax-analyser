2ae62575a265b162de296c0a873b212d8115c48a
Jonathan <jonathantheman@gmail.com>
2020-07-29 21:16:34 +1000

    Initial project skeleton.

---
+++ JackAnalyser.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JackAnalyser.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
