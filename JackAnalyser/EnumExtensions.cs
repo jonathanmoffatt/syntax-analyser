06ed91354e421b85d6c8d579f1c17a47db5aa711
Jonathan <jonathantheman@gmail.com>
2020-08-12 22:09:16 +1000

    Forgot to include these files to git

---
+++ JackAnalyser/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace JackAnalyser
+{
+    public static class EnumExtensions
+    {
+        public static T GetAttribute<T, TE>(this TE source) where TE : Enum
+        {
+            var enumType = typeof(TE);
+            var memberInfos = enumType.GetMember(source.ToString());
+            var enumValueMemberInfo = memberInfos.FirstOrDefault(m => m.DeclaringType == enumType);
+            var valueAttributes = enumValueMemberInfo.GetCustomAttributes(typeof(T), false);
+            return (T)valueAttributes[0];
+        }
+    }
+}
---
+++ JackAnalyser/NodeType.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace JackAnalyser
+{
+    public enum NodeType
+    {
+        [ElementName("class")]
+        Class,
+        [ElementName("classVarDec")]
+        ClassVariableDeclaration,
+        [ElementName("expression")]
+        Expression,
+        [ElementName("expressionList")]
+        ExpressionList,
+        [ElementName("identifier")]
+        Identifier,
+        [ElementName("ifStatement")]
+        IfStatement,
+        [ElementName("integerConstant")]
+        IntegerConstant,
+        [ElementName("keyword")]
+        Keyword,
+        [ElementName("letStatement")]
+        LetStatement,
+        [ElementName("parameterList")]
+        ParameterList,
+        [ElementName("returnStatement")]
+        ReturnStatement,
+        [ElementName("statements")]
+        Statements,
+        [ElementName("stringConstant")]
+        StringConstant,
+        [ElementName("subroutineBody")]
+        SubroutineBody,
+        [ElementName("subroutineDec")]
+        SubroutineDeclaration,
+        [ElementName("symbol")]
+        Symbol,
+        [ElementName("term")]
+        Term,
+        [ElementName("varDec")]
+        VariableDeclaration,
+        [ElementName("whileStatement")]
+        WhileStatement,
+        [ElementName("doStatement")]
+        DoStatement
+    }
+
+    public class ElementNameAttribute : Attribute
+    {
+        public ElementNameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
